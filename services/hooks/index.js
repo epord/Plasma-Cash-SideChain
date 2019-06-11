e06679d5276ffd4c53df5882d7e27cc29f082089
epord <pedro.balaguer@hotmail.com>
2019-06-11 16:52:21 -0300

    Events received

--- services/hooks/index.js
+++ services/hooks/index.js
@@ -3,32 +3,37 @@ const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:7545'
 const CryptoMonsJson = require("./CryptoMons.json");
 const _ = require('lodash');
 
-const subscribeLogEvent = (contract, eventName, cb) => {
-	const eventJsonInterface = _.find(
+const subscribeLogEvent = (contract, interface, cb) => {
+	const subscription = web3.eth.subscribe('logs', {
+	  address: contract.options.address,
+	  topics: [interface.signature]
+	}, cb)
+
+	console.log("Subscribed to event " + interface.name)
+}
+
+const getEventInterface = (contract, eventName) => {
+	return _.find(
 	  contract.jsonInterface.abi.events,
 	  o => o.name === eventName,
 	)
-	const subscription = web3.eth.subscribe('logs', {
-	  address: contract.options.address,
-	  topics: [eventJsonInterface.signature]
-	}, cb)
-
-	console.log("Subscribed to event " + eventName)
 }
 
 
 const init = () => {
 	const CryptoMonsContract = new web3.eth.Contract(CryptoMonsJson.abi,CryptoMonsJson.networks["5777"].address);
+	const interface = getEventInterface(CryptoMonsContract, 'Transfer');
 	// console.log(CryptoMonsContract);
-	subscribeLogEvent(CryptoMonsContract, 'Transfer', (error, result) => {
+	subscribeLogEvent(CryptoMonsContract, interface, (error, result) => {
 		console.log(error);
 		if (!error) {
+			console.log(interface.abiItem.inputs)
 		  const eventObj = web3.eth.abi.decodeLog(
-			eventJsonInterface.inputs,
-			result.data,
+			interface.abiItem.inputs,
+			result.data == "0x" ? undefined : result.data,
 			result.topics.slice(1)
 		  )
-		  console.log(`New ${eventName}!`, eventObj)
+		  console.log(`New Transfer!`, eventObj)
 		}
 	})
 }
