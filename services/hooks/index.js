e16a151e9969e34c2bc4c8a1e907ccd29d780d20
epord <pedro.balaguer@hotmail.com>
2019-07-30 21:38:13 -0300

    debug event working

--- services/hooks/index.js
+++ services/hooks/index.js
@@ -1,6 +1,7 @@
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:7545'));
 const CryptoMonsJson = require("./CryptoMons.json");
+const RootChainJson = require("./RootChain.json");
 const _ = require('lodash');
 
 const subscribeLogEvent = (contract, interface, cb) => {
@@ -21,9 +22,9 @@ const getEventInterface = (contract, eventName) => {
 
 
 const init = () => {
-	const CryptoMonsContract = new web3.eth.Contract(CryptoMonsJson.abi,CryptoMonsJson.networks["5777"].address);
-	const interface = getEventInterface(CryptoMonsContract, 'Transfer');
-	// console.log(CryptoMonsContract);
+	const RootChainContract = new web3.eth.Contract(RootChainJson.abi,RootChainJson.networks["5777"].address);
+
+	const interface = getEventInterface(RootChainContract, 'Debug');
 	subscribeLogEvent(CryptoMonsContract, interface, (error, result) => {
 		console.log(error);
 		if (!error) {
@@ -36,6 +37,23 @@ const init = () => {
 		  console.log(`New Transfer!`, eventObj)
 		}
 	})
+
+//	const CryptoMonsContract = new web3.eth.Contract(CryptoMonsJson.abi,CryptoMonsJson.networks["5777"].address);
+
+	// const interface = getEventInterface(CryptoMonsContract, 'Transfer');
+	// // console.log(CryptoMonsContract);
+	// subscribeLogEvent(CryptoMonsContract, interface, (error, result) => {
+	// 	console.log(error);
+	// 	if (!error) {
+	// 		console.log(interface.abiItem.inputs)
+	// 	  const eventObj = web3.eth.abi.decodeLog(
+	// 		interface.abiItem.inputs,
+	// 		result.data == "0x" ? undefined : result.data,
+	// 		result.topics.slice(1)
+	// 	  )
+	// 	  console.log(`New Transfer!`, eventObj)
+	// 	}
+	// })
 }
 
 module.exports = {
