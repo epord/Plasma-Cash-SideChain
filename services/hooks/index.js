4fdd2b60a8b21398eb1264b9a0fa2c3e09524485
epord <pedro.balaguer@hotmail.com>
2019-08-02 12:55:25 -0300

    Deposits are recevied and saved

--- services/hooks/index.js
+++ services/hooks/index.js
@@ -2,7 +2,9 @@ const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:7545'));
 const CryptoMonsJson = require("./CryptoMons.json");
 const RootChainJson = require("./RootChain.json");
+const BigNumber = require("bignumber.js");
 const _ = require('lodash');
+const { depositBlock }	= require('../../services/block');
 
 const subscribeLogEvent = (contract, interface, cb) => {
 	const subscription = web3.eth.subscribe('logs', {
@@ -23,37 +25,70 @@ const getEventInterface = (contract, eventName) => {
 
 const init = () => {
 	const RootChainContract = new web3.eth.Contract(RootChainJson.abi,RootChainJson.networks["5777"].address);
+	const CryptoMonContract = new web3.eth.Contract(CryptoMonsJson.abi,CryptoMonsJson.networks["5777"].address);
 
-	const interface = getEventInterface(RootChainContract, 'Debug');
-	subscribeLogEvent(CryptoMonsContract, interface, (error, result) => {
+	const idebugInterface = getEventInterface(RootChainContract, 'Debug');
+	subscribeLogEvent(RootChainContract, idebugInterface, (error, result) => {
 		console.log(error);
 		if (!error) {
-			console.log(interface.abiItem.inputs)
+			console.log(idebugInterface.abiItem.inputs)
 		  const eventObj = web3.eth.abi.decodeLog(
-			interface.abiItem.inputs,
+			idebugInterface.abiItem.inputs,
+			result.data == "0x" ? undefined : result.data,
+			result.topics.slice(1)
+		  )
+		  console.log(`Debug: `, eventObj)
+		}
+	})
+
+	const depositInterface = getEventInterface(RootChainContract, 'Deposit');
+	subscribeLogEvent(RootChainContract, depositInterface, (error, result) => {
+		console.log(error);
+		if (!error) {
+			console.log(depositInterface.abiItem.inputs)
+		  const eventObj = web3.eth.abi.decodeLog(
+				depositInterface.abiItem.inputs,
+				result.data == "0x" ? undefined : result.data,
+				result.topics.slice(1)
+			)
+
+			console.log(`Deposit: `, eventObj)
+			const { slot, blockNumber, from } = eventObj;
+			if (slot == undefined || !from || blockNumber == undefined) {
+				console.error('Missing parameter');
+				return;
+			}
+
+			const slotBN = new BigNumber(slot.toString());
+			if(slotBN.isNaN()) {
+				console.error('Invalid slot');
+				return;
+			}
+
+			const blockNumberBN = new BigNumber(blockNumber.toString());
+			if(blockNumberBN.isNaN()) {
+				console.error('Invalid blockNumber');
+				return;
+			}
+
+			// TODO: que hacer con errores??
+
+			depositBlock(slotBN, blockNumberBN, from, (err) => { console.error(err); });
+		}
+	})
+
+	const transferInterface = getEventInterface(CryptoMonContract, 'Transfer');
+	subscribeLogEvent(CryptoMonContract, transferInterface, (error, result) => {
+		console.log(error);
+		if (!error) {
+		  const eventObj = web3.eth.abi.decodeLog(
+			transferInterface.abiItem.inputs,
 			result.data == "0x" ? undefined : result.data,
 			result.topics.slice(1)
 		  )
 		  console.log(`New Transfer!`, eventObj)
 		}
 	})
-
-//	const CryptoMonsContract = new web3.eth.Contract(CryptoMonsJson.abi,CryptoMonsJson.networks["5777"].address);
-
-	// const interface = getEventInterface(CryptoMonsContract, 'Transfer');
-	// // console.log(CryptoMonsContract);
-	// subscribeLogEvent(CryptoMonsContract, interface, (error, result) => {
-	// 	console.log(error);
-	// 	if (!error) {
-	// 		console.log(interface.abiItem.inputs)
-	// 	  const eventObj = web3.eth.abi.decodeLog(
-	// 		interface.abiItem.inputs,
-	// 		result.data == "0x" ? undefined : result.data,
-	// 		result.topics.slice(1)
-	// 	  )
-	// 	  console.log(`New Transfer!`, eventObj)
-	// 	}
-	// })
 }
 
 module.exports = {
